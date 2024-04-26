666f71b3a32e5100994c9fbd21900585ebc43605
jgarcia <jgarcia@106-03.info2>
2024-04-26 11:11:03 +0200

    Hecho por mí

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -3,8 +3,6 @@ package prueba;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Prueba {
